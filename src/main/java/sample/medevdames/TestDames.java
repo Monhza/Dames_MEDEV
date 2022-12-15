03e53a1c708ea380f85fa915193532544280be62
Monhza <nicolas.thevenot@live.fr>
2022-12-15 11:19:50 +0100

    Nouveau jeu

--- src/main/java/sample/medevdames/TestDames.java
+++
@@ -1,9 +0,0 @@
-package sample.medevdames;
-
-public class TestDames {
-    public static final int TAILLE_PLATEAU = 8;
-
-    public static void main(String[] args) {
-        System.out.println("Hi fuckers");
-    }
-}
