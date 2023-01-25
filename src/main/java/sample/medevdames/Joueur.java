958c2d978e40edae0cb1afebf0134b5933fb68e1
Monhza <112829774+Monhza@users.noreply.github.com>
2023-01-25 11:08:34 +0100

    Update Joueur.java

--- src/main/java/sample/medevdames/Joueur.java
+++ src/main/java/sample/medevdames/Joueur.java
@@ -17,7 +17,6 @@ public class Joueur {
      * @param plateau plateau de jeu
      */
     public Joueur(Plateau plateau) {
-        this.petitNom = petitNom;
         this.plateau = plateau;
     }
 
